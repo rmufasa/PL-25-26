b135cd7e14ec7ec6cba0464a4dabfe2498819594
a104523 <a104523@alunos.uminho.pt>
2026-05-01 15:49:29 +0100

    test fix

--- src/tests/standard_test.py
+++ src/tests/standard_test.py
@@ -4,34 +4,6 @@ from SemanticAnalyzer import SemanticAnalyzer
 from CodeGenerator import CodeGenerator
 
 
-
-test_test_1 = """
-PROGRAM TESTFLOW
-INTEGER X
-
-X = 1
-
-10 IF (X .LE. 10) THEN
-
-    IF (X .GT. 8) THEN
-        PRINT *, X, ' is BIG'
-    ELSE
-        IF (X .EQ. 3 .OR. X .EQ. 7) THEN
-            PRINT *, X, ' is SPECIAL'
-        ELSE
-            PRINT *, X, ' is NORMAL'
-        ENDIF
-    ENDIF
-
-    X = X + 1
-    GOTO 10
-
-ENDIF
-
-PRINT *, 'DONE'
-END
-"""
-
 test_4 = """
 PROGRAM SOMAARR
 INTEGER NUMS(5)
